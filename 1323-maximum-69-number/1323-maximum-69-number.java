1e2f2b2642071e52662b90b62022ee85de54dfc8
Oğuz Kağan EREN <me@oguzkaganeren.com.tr>
2022-11-16 23:08:12 +0300

    Time: 8 ms (13.65%), Space: 41.2 MB (38.03%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String numStr=String.valueOf(num);
+        numStr=numStr.replaceFirst("6","9");
+        return Integer.valueOf(numStr);
+    }
+}
