4154b21b503b38b1a97dcc4aa91063599889a204
Oğuz Kağan EREN <me@oguzkaganeren.com.tr>
2022-11-16 23:14:17 +0300

    Time: 1073 ms (98.18%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select v.customer_id,count(1) as count_no_trans
+from Visits v
+where v.visit_id not in (select visit_id from Transactions)
+group by v.customer_id
+
+
