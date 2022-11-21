b5610252ce52004701b4a5ad6744e4aa3bedf0a7
Oğuz Kağan EREN <me@oguzkaganeren.com.tr>
2022-11-21 08:09:31 +0300

    Time: 0 ms (100.00%), Space: 40.9 MB (82.38%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] sumZero(int n) {
         int[] res=new int[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n/2;i++){
             res[i]=2*i-n+1;
+            res[n-i-1]=2*(n-i-1)-n+1;
         }
         return res;
     }
